d5fc458a59f3899c4df631a2e48ec98d60621c79
Cristina Laryea <crispad06@gmail.com>
2019-06-03 16:26:34 -0400

    added state, rendering on local host

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -1,16 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 
-const App = () => {
-    window.navigator.geolocation.getCurrentPosition(
-        (position) => console.log(position),
-        (err) => console.log(err)
-    );
 
-    return <div>Hi there, testing!</div>;
-};
+class App extends React.Component {
+    constructor(props) {
+        super(props);
 
-ReactDOM.render(
-    <App />,
-    document.querySelector('#root')
-);
+        this.state = { lat: 40}
+        window.navigator.geolocation.getCurrentPosition(
+            position => console.log(position),
+            err => console.log(err)
+        );
+    }
+
+
+  render() {
+    
+  
+    return <div>Latitude: {this.state.lat} </div>;
+  }
+}
+
+ReactDOM.render(<App />, document.querySelector("#root"));
