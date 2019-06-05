850729465b4675b89bd8d169a01acc8a10b5b83f
Cristina Laryea <crispad06@gmail.com>
2019-06-04 23:28:12 -0400

    added componentDidMount

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -8,14 +8,13 @@ class App extends React.Component {
 
         this.state = { lat: null, errorMessage: '' };
         
+    }
+
+    componentDidMount() {
         window.navigator.geolocation.getCurrentPosition(
-            position => {
-                this.setState({ lat: position.coords.latitude });
-            },
-          err => {
-              this.setState({ errorMessage: err.message });
-          }
-        );
+            position => this.setState({ lat: position.coords.latitude }),
+            err => this.setState({ errorMessage: err.message })
+          );     
     }
 
 
