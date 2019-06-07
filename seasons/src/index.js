2b91da5ee18412c6e7df44ff1ac6b2d477f7b42b
Cristina Laryea <crispad06@gmail.com>
2019-06-06 23:19:28 -0400

    loading spinner rendering

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import SeasonDisplay from './SeasonDisplay';
-
+import Spinner from './Spinner';
 
 class App extends React.Component {
     state = { lat: null, errorMessage: '' };
@@ -22,7 +22,7 @@ class App extends React.Component {
     if (!this.state.errorMessage && this.state.lat) {
         return <SeasonDisplay lat={ this.state.lat } />
     }
-        return <div>Loading!</div>;
+        return <Spinner />;
   }
 }
 
